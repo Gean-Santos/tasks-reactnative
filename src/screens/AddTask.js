fa44ca3233988247fbce9f825048cbc0474ea170
Gean-Santos <geansantos_15@hotmail.com>
2020-04-18 16:34:48 -0300

    editing Task modal

--- src/screens/AddTask.js
+++ src/screens/AddTask.js
@@ -2,9 +2,12 @@ import React, { Component } from 'react';
 import { 
   Modal, 
   View, 
+  Text,
   StyleSheet,
   TouchableWithoutFeedback, 
 } from 'react-native';
+
+import commonStyles from '../commonStyles';
 
 export default class AddTask extends Component {
   render() {
@@ -12,8 +15,15 @@ export default class AddTask extends Component {
       <Modal transparent={true} 
         visible={this.props.isVisible}
         onRequestClose={this.props.onCancel}
-        animationType='slide'
-      >
+        animationType='slide'>
+
+        <TouchableWithoutFeedback
+          onPress={this.props.onCancel}>
+          <View style={styles.background}></View>
+        </TouchableWithoutFeedback>
+        <View style={styles.container}>
+          <Text style={styles.header}>Nova Tarefa</Text>
+        </View>
         <TouchableWithoutFeedback
           onPress={this.props.onCancel}>
           <View style={styles.background}></View>
@@ -27,5 +37,17 @@ const styles = StyleSheet.create({
   background: {
     flex: 1,
     backgroundColor: 'rgba(0, 0, 0, 0.7)',
+  },
+  container: {
+    flex: 1,
+    backgroundColor: '#FFF',
+  },
+  header: {
+    fontFamily: commonStyles.fontFamily,
+    backgroundColor: commonStyles.colors.today,
+    color: commonStyles.colors.secondary,
+    textAlign: 'center',
+    padding: 15,
+    fontSize: 18,
   },
 })
