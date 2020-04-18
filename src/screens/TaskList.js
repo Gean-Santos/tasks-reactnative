9b5f5b892676c6f26fcd64b70df5babd9f966463
Gean-Santos <geansantos_15@hotmail.com>
2020-04-18 17:10:55 -0300

    Add AddTask button and function

--- src/screens/TaskList.js
+++ src/screens/TaskList.js
@@ -19,11 +19,12 @@ import 'moment/locale/pt-br';
 
 import Task from '../components/Task';
 import AddTask from './AddTask';
+import commonStyles from '../commonStyles';
 
 export default class TaskList extends Component {
   state = {
     showDoneTasks: true,
-    showAddTask: true,
+    showAddTask: false,
     visibleTasks: [],
     tasks: [{
       id: Math.random(),
@@ -99,6 +100,12 @@ export default class TaskList extends Component {
             }
           />
         </View>
+        <TouchableOpacity 
+          style={styles.addButton}
+          onPress={() => this.setState({showAddTask: true})}
+          activeOpacity={0.7}>
+          <Icon name="plus" size={24} color={commonStyles.colors.secondary} />
+        </TouchableOpacity>
       </View>
     );
   }
@@ -137,5 +144,16 @@ const styles = StyleSheet.create({
     marginHorizontal: 20,
     justifyContent: 'flex-end',
     marginTop: Platform.OS === 'ios' ? 40 : 10,
+  },
+  addButton: {
+    position: 'absolute',
+    right: 30,
+    bottom: 30,
+    width: 50,
+    height: 50,
+    borderRadius: 25,
+    backgroundColor: commonStyles.colors.today,
+    justifyContent: 'center',
+    alignItems: 'center',
   },
 });
