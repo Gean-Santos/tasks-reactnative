fb4109a89e5aeadf081e2e3f808dc7e9bfdc8d65
Gean-Santos <geansantos_15@hotmail.com>
2020-04-18 16:57:39 -0300

    finishing AddTask Screen

--- src/screens/AddTask.js
+++ src/screens/AddTask.js
@@ -3,13 +3,22 @@ import {
   Modal, 
   View, 
   Text,
+  TouchableOpacity,
+  TextInput,
   StyleSheet,
   TouchableWithoutFeedback, 
 } from 'react-native';
 
 import commonStyles from '../commonStyles';
 
+const initialState = { desc: '' };
+
 export default class AddTask extends Component {
+
+  state = {
+    ...initialState
+  };
+
   render() {
     return (
       <Modal transparent={true} 
@@ -23,6 +32,19 @@ export default class AddTask extends Component {
         </TouchableWithoutFeedback>
         <View style={styles.container}>
           <Text style={styles.header}>Nova Tarefa</Text>
+          <TextInput 
+            style={styles.input} 
+            placeholder="Informe a descrição..."
+            value={this.state.desc}
+            onChangeText={desc => this.setState({desc})}/>
+          <View style={styles.buttons}> 
+            <TouchableOpacity onPress={this.props.onCancel}>
+              <Text style={styles.button}>Cancel</Text>
+            </TouchableOpacity>
+            <TouchableOpacity >
+              <Text style={styles.button}>Salvar</Text>
+            </TouchableOpacity>
+          </View>
         </View>
         <TouchableWithoutFeedback
           onPress={this.props.onCancel}>
@@ -39,7 +61,6 @@ const styles = StyleSheet.create({
     backgroundColor: 'rgba(0, 0, 0, 0.7)',
   },
   container: {
-    flex: 1,
     backgroundColor: '#FFF',
   },
   header: {
@@ -49,5 +70,23 @@ const styles = StyleSheet.create({
     textAlign: 'center',
     padding: 15,
     fontSize: 18,
+  },
+  input: {
+    fontFamily: commonStyles.fontFamily,
+    height: 40, 
+    margin: 15,
+    backgroundColor: '#FFF',
+    borderWidth: 1,
+    borderColor: '#E3E3E3', 
+    borderRadius: 6,
+  },
+  buttons: {
+    flexDirection: 'row',
+    justifyContent: 'flex-end',
+  },
+  button: {
+    margin: 20, 
+    marginRight: 30,
+    color: commonStyles.colors.today,
   },
 })
